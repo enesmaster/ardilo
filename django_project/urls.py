bdec9e0708e88f35f496e1212a003e5c0e59a16a
enes <=>
2022-05-24 00:32:02 +0300

    dark mode

--- django_project/urls.py
+++ django_project/urls.py
@@ -3,10 +3,10 @@ from django.urls import path,include
 from django.contrib.auth import views as auth_views
 import web.urls
 import api.urls
-
+handler404 = 'web.views.not_found404'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api.urls)),
     path('', include(web.urls)),
-]
+    ]
